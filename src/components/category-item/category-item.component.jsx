22c530fba776c6cb4cb23ba9927378a828904304
Gerard Press <killerabs@gmail.com>
2023-02-03 06:35:48 +0000

    font changes

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -4,12 +4,11 @@ const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
     <div className="category-container">
-      <img
+      <div
         className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
-        alt={`${title}`}
       />
       <div className="category-body-container">
         <h2>{title}</h2>
