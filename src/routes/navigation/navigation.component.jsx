4adf4b34c2b1069baff9526b2bb92292a11037f5
Gerard Press <killerabs@gmail.com>
2023-02-21 07:15:51 +0000

    product cards and shopping bag icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,8 @@ import { Fragment, useContext } from "react";
 import { ReactComponent as Logo } from "../../assets/topphoto.svg";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import "./navigation.style.scss";
 
 const Navigation = () => {
@@ -27,7 +29,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
