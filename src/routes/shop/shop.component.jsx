dcaee9b689ad7d0d328d164a9dff1a7615edea6e
Gerard Press <killerabs@gmail.com>
2023-03-05 15:44:02 +0000

    categoty cards and routes

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,12 +1,18 @@
-import { Fragment, useContext } from "react";
+// import { Fragment, useContext } from "react";
 import "./shop.styles.scss";
 
-import { CategoriesContext } from "../../contexts/categories.context";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+// import { CategoriesContext } from "../../contexts/categories.context";
+// import CategoryPreview from "../../components/category-preview/category-preview.component";
 // import ProductCard from "../../components/product-card/product-card.component";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-previews.component";
+import Category from "../category/category.component";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+  /*******one of the ways of creating category preview
+    /*
+    {
+      const { categoriesMap } = useContext(CategoriesContext);
   return (
     <div className="shop-container">
       {Object.keys(categoriesMap).map((title) => {
@@ -15,19 +21,27 @@ const Shop = () => {
           <CategoryPreview key={title} title={title} products={products} />
         );
 
-        /****** below is the code used if there is no category preview *******/
-        {
-          /* <Fragment key={title}>
+
+
+        /****** below is the code used if there is no category preview 
+         <Fragment key={title}>
           <h2>{title}</h2>
           <div className="products-container">
             {categoriesMap[title].map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
           </div>
-        </Fragment> */
+        </Fragment> 
         }
       })}
     </div>
+  );}
+  */ return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
+
 export default Shop;
